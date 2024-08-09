cbcc8bfd003abff4f7421ac2292bf2d92d8bc05a
Chris <2293611667@qq.com>
2024-08-09 15:16:20 +0800

    modify generate-stackbrew-library.sh

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -2,15 +2,15 @@
 set -Eeuo pipefail
 
 declare -A aliases=(
-    [16]='latest'
+	[16]='latest'
 )
 
 self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 if [ "$#" -eq 0 ]; then
-    versions="$(jq -r 'keys | map(@sh) | join(" ")' versions.json)"
-    eval "set -- $versions"
+	versions="$(jq -r 'keys | map(@sh) | join(" ")' versions.json)"
+	eval "set -- $versions"
 fi
 
 # sort version numbers with highest first
@@ -18,34 +18,33 @@ IFS=$'\n'; set -- $(sort -rV <<<"$*"); unset IFS
 
 # get the most recent commit which modified any of "$@"
 fileCommit() {
-    git log -1 --format='format:%H' HEAD -- "$@"
+	git log -1 --format='format:%H' HEAD -- "$@"
 }
 
 # get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
 dirCommit() {
-    local dir="$1"; shift
-    (
-        cd "$dir"
-        files="$(
-            git show HEAD:./Dockerfile | awk '
-                toupper($1) == "COPY" {
-                    for (i = 2; i < NF; i++) {
-                        if ($i ~ /^--from=/) {
-                            next
-                        }
-                        print $i
-                    }
-                }
-            '
-        )"
-        fileCommit Dockerfile $files
-    )
+	local dir="$1"; shift
+	(
+		cd "$dir"
+		files="$(
+			git show HEAD:./Dockerfile | awk '
+				toupper($1) == "COPY" {
+					for (i = 2; i < NF; i++) {
+						if ($i ~ /^--from=/) {
+							next
+						}
+						print $i
+					}
+				}
+			'
+		)"
+		fileCommit Dockerfile $files
+	)
 }
-
 getArches() {
     local repo="$1"; shift
     local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/debian'
-
+	print "'"$officialImagesUrl"'"
     # Fetch the content from the fixed URL and process it
     eval "declare -g -A parentRepoToArches=( $(
         curl -s "$officialImagesUrl" \
@@ -53,9 +52,6 @@ getArches() {
     ) )"
 }
 getArches 'postgres'
-
-
-
 
 cat <<-EOH
 # this file is generated via https://github.com/docker-library/postgres/blob/$(fileCommit "$self")/$self
@@ -67,72 +63,64 @@ EOH
 
 # prints "$2$1$3$1...$N"
 join() {
-    local sep="$1"; shift
-    local out; printf -v out "${sep//%/%%}%s" "$@"
-    echo "${out#$sep}"
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
 }
 
 for version; do
-    export version
+	export version
 
-    variants="$(jq -r '.[env.version].variants | map(@sh) | join(" ")' versions.json)"
-    eval "variants=( $variants )"
+	variants="$(jq -r '.[env.version].variants | map(@sh) | join(" ")' versions.json)"
+	eval "variants=( $variants )"
 
-    alpine="$(jq -r '.[env.version].alpine' versions.json)"
-    debian="$(jq -r '.[env.version].debian' versions.json)"
+	alpine="$(jq -r '.[env.version].alpine' versions.json)"
+	debian="$(jq -r '.[env.version].debian' versions.json)"
 
-    fullVersion="$(jq -r '.[env.version].version' versions.json)"
+	fullVersion="$(jq -r '.[env.version].version' versions.json)"
 
-    # ex: 9.6.22, 13.3, or 14beta2
-    versionAliases=(
-        $fullVersion
-    )
-    # skip unadorned "version" on prereleases: https://www.postgresql.org/developer/beta/
-    # ex: 9.6, 13, or 14
-    case "$fullVersion" in
-        *alpha* | *beta* | *rc*) ;;
-        *) versionAliases+=( $version ) ;;
-    esac
-    # ex: 9 or latest
-    versionAliases+=(
-        ${aliases[$version]:-}
-    )
+	# ex: 9.6.22, 13.3, or 14beta2
+	versionAliases=(
+		$fullVersion
+	)
+	# skip unadorned "version" on prereleases: https://www.postgresql.org/developer/beta/
+	# ex: 9.6, 13, or 14
+	case "$fullVersion" in
+		*alpha* | *beta* | *rc*) ;;
+		*) versionAliases+=( $version ) ;;
+	esac
+	# ex: 9 or latest
+	versionAliases+=(
+		${aliases[$version]:-}
+	)
 
-    for variant in "${variants[@]}"; do
-        dir="$version/$variant"
-        commit="$(dirCommit "$dir")"
+	for variant in "${variants[@]}"; do
+		dir="$version/$variant"
+		commit="$(dirCommit "$dir")"
 
-        # Handle FROM with --platform parameter correctly
-        parent="$(awk '
-            toupper($1) == "FROM" {
-                base_image=""
-                platform=""
-                for (i = 2; i <= NF; i++) {
-                    if ($i ~ /^--platform=/) {
-                        platform=$i
-                    } else {
-                        base_image=$i
-                    }
-                }
-                print base_image, platform
-            }
-        ' "$dir/Dockerfile")"
+		parent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
+		arches="amd64"
 
-        parent_image="$(echo "$parent" | awk '{print $1}')"
-        platform="$(echo "$parent" | awk '{print $2}')"
-        arches="amd64"
+		variantAliases=( "${versionAliases[@]/%/-$variant}" )
+		variantAliases=( "${variantAliases[@]//latest-/}" )
 
-        variantAliases=( "${versionAliases[@]/%/-$variant}" )
-        variantAliases=( "${variantAliases[@]//latest-/}" )
-
-        echo
-        cat <<-EOE
-            Tags: $(join ', ' "${variantAliases[@]}")
-            Architectures: $(join ', ' $arches)
-            GitCommit: $commit
-            Directory: $dir
-            BaseImage: $parent_image
-            Platform: $platform
-        EOE
-    done
+		echo
+		cat <<-EOE
+			Tags: $(join ', ' "${variantAliases[@]}")
+			Architectures: $(join ', ' $arches)
+			GitCommit: $commit
+			Directory: $dir
+		EOE
+	done
 done
+getArches() {
+    local repo="$1"; shift
+    local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/debian'
+
+    # Fetch the content from the fixed URL and process it
+    eval "declare -g -A parentRepoToArches=( $(
+        curl -s "$officialImagesUrl" \
+        | xargs bashbrew cat --format '[{{ .RepoName }}:{{ .TagName }}]="{{ join " " .TagEntry.Architectures }}"'
+    ) )"
+}
+getArches 'postgres'
