35ba2b4435f29a4dae93489cc37f591611dc09ed
Chris <2293611667@qq.com>
2024-08-10 18:28:49 +0800

    Add support for multi-architecture in Dockerfile

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -48,9 +48,8 @@ getArches() {
 
 	eval "declare -g -A parentRepoToArches=( $(
 		find -name 'Dockerfile' -exec awk '
-				toupper($1) == "FROM" && $3 !~ /^('"$repo"'|scratch|.*\/.*)(:|$)/ {
-					echo  "'"$officialImagesUrl"'" $3
-					print "'"$officialImagesUrl"'" $3
+				toupper($1) == "FROM" && $2 !~ /^('"$repo"'|scratch|.*\/.*)(:|$)/ {
+					print "'"$officialImagesUrl"'" $2
 				}
 			' '{}' + \
 			| sort -u \
@@ -104,7 +103,7 @@ for version; do
 		dir="$version/$variant"
 		commit="$(dirCommit "$dir")"
 
-		parent="$(awk 'toupper($1) == "FROM" { print $3 }' "$dir/Dockerfile")"
+		parent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
 		arches="amd64"
 
 		variantAliases=( "${versionAliases[@]/%/-$variant}" )
